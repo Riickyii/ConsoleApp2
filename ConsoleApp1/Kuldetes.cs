9b509c77ed1b4bcc1a5be7ca97d2f7499f546463
krausz.miron <krausz.miron@28-17>
2022-09-20 10:27:55 +0200

    Add project files.

---
+++ ConsoleApp1/Kuldetes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Kuldetes
+    {
+        public string Kod { get; set; }
+        public DateOnly KilovesNapja { get; set; }
+        public string SikoNeve { get; set; }
+        private int KuldetesenToltottNap { get; set; }
+        private int KuldetesenToltottOra { get; set; }
+
+        public int KuldetesHossza => KuldetesenToltottNap * 24 + KuldetesenToltottOra;
+
+        public string ErkezesHelye { get; set; }
+        public int UtasokSzama { get; set; }
+        public Kuldetes(string fileEgySora)
+        {
+            var adattagok = fileEgySora.Split(";");
+            Kod = adattagok[0];
+            KilovesNapja = DateOnly.Parse(adattagok[1]);
+            SikoNeve = adattagok[2];
+            KuldetesenToltottNap = int.Parse(adattagok[3]);
+            KuldetesenToltottOra = int.Parse(adattagok[4]);
+            ErkezesHelye = adattagok[5];
+            UtasokSzama = int.Parse(adattagok[6]);
+        }
+     }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1;
+
+List<Kuldetes> kuldetesek = new();
+using StreamReader sr = new(@"..\..\..\res\kuldetesek.csv");
+while (!sr.EndOfStream) kuldetesek.Add(new Kuldetes(sr.ReadLine()));
+
+Console.WriteLine($"3. Feladat: \n\tösszesen {kuldetesek.Count} űrhajó lett indítva.");
+
+
+int UtasokSzamanakOsszege = kuldetesek.Sum(x => x.UtasokSzama);
+Console.WriteLine($"4. Feladat: \n\t{UtasokSzamanakOsszege} utas indult az űrbe összesen.");
+
+
+int k5u = kuldetesek.Count(x => x.UtasokSzama < 7);
+Console.WriteLine($"5. Feladat: \n\t" +
+                  $"Összesen {k5u} alkalommal küldtek kevesebb mint 5 embert az űrbe.");
